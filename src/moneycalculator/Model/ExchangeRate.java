74690090330d38e539f98c9696b308f9a50b59fc
Eleazar <Eleazar@Eleazar-PC>
2014-01-09 17:21:53 +0000

    Model-2

---
+++ src/moneycalculator/Model/ExchangeRate.java
@@ -0,0 +1,36 @@
+package moneycalculator.Model;
+
+import java.util.Date;
+
+public class ExchangeRate {
+    private final Date date;
+    private final Currency from;
+    private final Currency to;
+    private final double rate;
+
+    public ExchangeRate(Date date, Currency from, Currency to, double rate) {
+        this.date = date;
+        this.from = from;
+        this.to = to;
+        this.rate = rate;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public Currency getFrom() {
+        return from;
+    }
+
+    public Currency getTo() {
+        return to;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+    
+    
+    
+}
